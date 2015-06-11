c83ddbeb8e04c20e8a851afe37efb0cf3cebbe83
Adam Johnson <adam.github@outlook.com>
2015-06-11 17:22:27 -0500

    Load and retrieve data from Parse.

--- InstaMimic/HomeFeedViewController.m
+++ InstaMimic/HomeFeedViewController.m
@@ -9,6 +9,7 @@
 #import "HomeFeedViewController.h"
 
 @interface HomeFeedViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
 
 @end
 
@@ -17,16 +18,108 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    
-    
-    
-    PFObject *gameScore = [PFObject objectWithClassName:@"Game Time"];
-    gameScore[@"score"] = @1337;
-    gameScore[@"playerName"] = @"Adam";
-    gameScore[@"cheat"] = @NO;
-    
 
-    // Do any additional setup after loading the view.
+    // Stores image in variable braveheart.
+    UIImage *braveheart = [UIImage imageNamed:@"braveheart"];
+
+    // Convert to JPEG with 50% quality
+    NSData* data = UIImageJPEGRepresentation(braveheart, 0.5f);
+
+    PFFile *imageFile = [PFFile fileWithName:@"Image.jpg" data:data];
+
+    // Save the image to Parse
+
+    [imageFile saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+        if (!error) {
+            // The image has now been uploaded to Parse. Associate it with a new object
+            PFObject* newPhotoObject = [PFObject objectWithClassName:@"PhotoObject"];
+            [newPhotoObject setObject:imageFile forKey:@"image"];
+
+            [newPhotoObject saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+                if (!error) {
+                    NSLog(@"Saved");
+                }
+                else{
+                    // Error
+                    NSLog(@"Error: %@ %@", error, [error userInfo]);
+                }
+            }];
+        }
+    }];
+
+    [imageFile getDataInBackgroundWithBlock:^(NSData *data, NSError *error)
+    {
+        PFQuery *query = [PFQuery queryWithClassName:@"PhotoObject"];
+        [query getObjectInBackgroundWithId:@"fbRKhhGpMW" block:^(PFObject *newPhotoObject, NSError *error)
+        {
+
+            if (!error)
+            {
+                PFFile *imageFile2 = [newPhotoObject objectForKey:@"image"];
+                [imageFile2 getDataInBackgroundWithBlock:^(NSData *data, NSError *error)
+                {
+                    if (!error)
+                    {
+                        self.imageView.image = [UIImage imageWithData:data];
+                    }
+                }];
+            }
+
+        }];
+    }];
+
+}
+    //[object saveInBackground];
+
+
+//    gameScore[@"playerName"] = @"Adam";
+//    gameScore[@"cheat"] = @NO;
+//    gameScore[@"name"] = @"Player 1";
+
+    // Saves image to Parse.
+    //[image saveInBackground];
+
+//    PFQuery *query = [PFQuery queryWithClassName:@"GameTime"];
+//    [query getObjectInBackgroundWithId:@"TgyDin75ZI" block:^(PFObject *gameScore, NSError *error)
+//
+//    {
+//        NSLog(@"%@", gameScore);
+//    }];
+
+//    PFQuery *query2 = [PFQuery queryWithClassName:@"GameTime"];
+//    [query2 findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error)
+//    {
+//        if (!error)
+//        {
+//            // The find succeeded. The first 100 objects are available in objects
+//            NSLog(@"%@", objects);
+//
+//            for (PFObject *pfob in objects)
+//            {
+//                //NSLog(@"%@", pfob[@"name"]);
+//                NSLog(@"%@", pfob[@"playerName"]);
+//
+//
+//
+//            }
+//
+//        }
+//        else
+//        {
+//            // Log details of the failure
+//            NSLog(@"Error: %@ %@", error, [error userInfo]);
+//        }
+//    }];
+
+
+//    int score = [[gameScore objectForKey:@"score"] intValue];
+//    NSString *playerName = gameScore[@"playerName"];
+//    BOOL cheatMode = [gameScore ]
+
+
+
+
+// Do any additional setup after loading the view.
 //    PFUser *user = [PFUser user];
 //    user.username = @"my name";
 //    user.password = @"my pass";
@@ -62,15 +155,13 @@
         //else {
 //        [self performSegueWithIdentifier:@"LoginSegue" sender:self];
 //    }
-}
+
 
 //-(void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
 //    if ([PFUser currentUser] == nil) {
 //        [self performSegueWithIdentifier:@"BackToLogInSegue" sender:sender];
 //    } 
 //}
-
-
 
 
 
