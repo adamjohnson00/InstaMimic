967a9027037f92fdf69e2bc81a8bac3b5e3e34cb
Brittany Kimbrough <bkimbrough87@gmail.com>
2015-06-11 11:49:29 -0500

    Added parse

--- InstaMimic/HomeFeedViewController.m
+++ InstaMimic/HomeFeedViewController.m
@@ -17,12 +17,50 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+//    PFUser *user = [PFUser user];
+//    user.username = @"my name";
+//    user.password = @"my pass";
+//    user.email = @"email@example.com";
+//    
+//    // other fields can be set if you want to save more information
+//    user[@"phone"] = @"650-555-0000";
+//    
+//    PFUser* user = [PFUser logInWithUsername:@"my name" password:@"my pass" error:nil];
+//    
+//    if (user == nil) {
+//        //
+//    }
+//    else{
+//        [self performSegueWithIdentifier:@"BackToLogInSegue" sender:self];
+//    }
+    
+    
+//    [user signUpInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+//        if (!error) {
+//            NSLog(@"This user signed in correctly");
+//        } else {
+//            NSString *errorString = [error userInfo][@"error"];
+//            // Show the errorString somewhere and let the user try again.
+//            NSLog(@"%@", errorString);
+//        }
+//    }];
+//    if ([PFUser currentUser] == nil) {
+//        [self performSegueWithIdentifier:@"BackToLogInSegue" sender:self];
+//    } else {
+//        NSLog(@"Go to HomeFeedViewController");
+//    }
+        //else {
+//        [self performSegueWithIdentifier:@"LoginSegue" sender:self];
+//    }
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
+//-(void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+//    if ([PFUser currentUser] == nil) {
+//        [self performSegueWithIdentifier:@"BackToLogInSegue" sender:sender];
+//    } 
+//}
+
+
 
 /*
 #pragma mark - Navigation
---
+++ InstaMimic/LogInViewController.h
@@ -0,0 +1,13 @@
+//
+//  LogInViewController.h
+//  InstaMimic
+//
+//  Created by Brittany Kimbrough on 6/10/15.
+//  Copyright (c) 2015 Adam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LogInViewController : UIViewController
+
+@end
